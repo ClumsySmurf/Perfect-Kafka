cdf0e39f0a94f180b3eb99b6fd89d01c4c5de03f
John Hamilton <clumsysmurf30@gmail.com>
2019-06-28 10:27:06 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     name: "PerfectKafka",
      dependencies:[
       .package(url: "https://github.com/ClumsySmurf/Perfect-libKafka.git", .branch("master"))
-    ]
+    ],
     targets: [
         Target(name: "PerfectKefka")
     ]
